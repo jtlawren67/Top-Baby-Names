bb60c61bc73fe10f6e1c7eb01e4620bcb43eab68
jtlawren <joslawre@indiana.edu>
2020-05-23 16:42:22 -0400

    creating male version of chart

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 In discussing potential children's name, I decided to make this play project to make a racing barchart to see the Top 10 Girl's Names from 1880 to 2018.  The data comes from the [Social Security Administration](https://www.ssa.gov/oact/babynames/names.zip)
 
-The rough draft version of this chart is:
-!(https://github.com/jtlawren67/Top-Baby-Names/blob/master/baby_girls.gif)
+The rough draft version of this chart for Girl's names is:
+![](baby_girls.gif)
+
+And for boy's names:
+![](baby_boys.gif)
--- racing_chart_baby_names.R
+++ racing_chart_baby_names.R
@@ -12,43 +12,69 @@ all_data <- map_dfr(dir('.data'), function(x){
     mutate(year = x %>% str_extract('\\d+') %>% as.integer)
 })
 
-##Create Column For Cumulative Counts
-all_data_cumulative <- all_data %>%
-  group_by(gender, name) %>%
-  arrange(year) %>% 
-  mutate(cuml_count = cumsum(count)) %>%
-
 #Get Top 10 Per Year
-top_10_names <- all_data_cumulative %>% 
+top_10_names <- all_data %>% 
   group_by(gender, year) %>%
   top_n(10, wt = count) %>% 
   mutate(rank = rank(count))
 
 
-##Create Animated Racing Histogram Plot
-animated <- top_10_names %>% 
-  filter(gender == "F") %>%
-  ggplot(aes(x = rank, y = count/2, group = name)) + 
-    geom_col(fill = "#FFC0CB") + 
-    geom_text(aes(label = count %>% comma(accuracy = 1), hjust = 0)) + 
-    geom_text(aes(label = name), y = 0, vjust = .2, hjust = 1) +
-    labs(x = "Girl's Name", y = "# of Babies",
-         title = "Top 10 Girls Baby Names (1880-2018)",
+gen_graph <- function(cond, output_file){
+  
+  if(cond == "F"){
+    lbl = "Girl"
+    col = "#FFC0CB"
+  }else{
+    lbl = "Boy"
+    col = "#89cff0"
+  }
+  
+  animated <- top_10_names %>% 
+    filter(gender == cond) %>%
+    ggplot(aes(x = rank, y = count/2, group = name)) + 
+    geom_col(fill = col) + 
+    geom_text(aes(label = count %>% comma(accuracy = 1)), hjust = 0, size = 10) + 
+    geom_text(aes(label = name), y = 0, vjust = .2, hjust = 1, size = 10) +
+    labs(x = paste0(lbl,"'s Name"), y = "# of Babies",
+         title = paste0("Top 10 ", lbl, "'s Baby Names (1880-2018)"),
          subtitle = '{round(frame_time,0)}',
          caption = 'Source: Social Security Administration') + 
     coord_flip(clip = 'off') + 
-    cowplot::theme_cowplot() + 
-    theme(
-      axis.title = element_blank(),
-      axis.text.x = element_blank(), 
-      axis.ticks = element_blank(),
-      axis.text.y = element_blank() ,
-      axis.line=element_blank(),
-      plot.title.position = "plot"
-    ) + 
+    theme_minimal() +
+    theme(axis.line=element_blank(),
+          axis.text.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          legend.position="none",
+          panel.background=element_blank(),
+          panel.border=element_blank(),
+          panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),
+          panel.grid.major.x = element_line(size=.4, 
+                                            color="grey" ),
+          panel.grid.minor.x = element_line(size=.1, 
+                                            color="grey" ),
+          plot.title.position = "plot",
+          plot.title=element_text(size=20, 
+                                  face="bold", 
+                                  colour="#313632"),
+          plot.subtitle=element_text(size=50, 
+                                     color="#a3a5a8"),
+          plot.caption =element_text(size=15, 
+                                     color="#313632"),
+          plot.background=element_blank(),
+          plot.margin = margin(1, 9, 1, 9, "cm")) + 
     transition_time(year) + 
-    ease_aes('cubic-in-out') 
+    ease_aes('cubic-in-out') +
+    view_follow(fixed_x = T)
 
-animate(animated, fps = 10, duration = 50, width = 1000, height = 600,
-        renderer=gifski_renderer("baby_girls.gif"))
+  animate(animated, fps = 10, duration = 50, width = 1000, height = 600, 
+          end_pause = 50, start_pause = 50,
+          renderer=gifski_renderer(output_file))
+    
+}
 
+gen_graph("F", "baby_girls.gif")
+gen_graph("M", "baby_boys.gif")
